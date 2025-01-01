affe53389168e4b0ac840bcd83594b9641f3be64
Meteor_Kai <1318723916@qq.com>
2025-01-01 08:28:58 +0800

    Create errors.py

---
+++ pysimplesocket/errors.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# @FileName  :errors.py
+# @Time      :2023/11/22 12:37:43
+# @Author    :D0WE1L1N
+'''
+Definate all the errors
+'''
+class MessageLengthError(Exception):
+    def __init__(self, message) -> None:
+        self.message = message
